6e5f8220fd56517b011781f6fc92f970b4fc447a
masum9100 <masum.seopro@gmail.com>
2023-11-07 17:00:16 +0600

    filter data by user email

--- index.js
+++ index.js
@@ -15,60 +15,72 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const serviceCollection = client.db('pestControl').collection('services')
-    const bookingCollection = client.db('pestControl').collection('bookings')
+        const serviceCollection = client.db('pestControl').collection('services')
+        const bookingCollection = client.db('pestControl').collection('bookings')
 
-    app.get('/services', async(req, res)=>{
-        const cursor = serviceCollection.find()
-        const result = await cursor.toArray()
-        res.send(result)
-    })
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
 
-    app.get('/services/:id', async(req, res)=>{
-        const id = req.params.id
-        const query = {_id: new ObjectId(id)}
-        const options ={
-            projection: {name: 1, short_description: 1, details1: 1, details2: 1, details3: 1, image1: 1, image2: 1, image3: 1, price: 1, service_provider_name: 1, service_provider_image: 1, service_provider_email: 1}
-        }
-        const result = await serviceCollection.findOne(query, options)
-        res.send(result)
-    })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const options = {
+                projection: { name: 1, short_description: 1, details1: 1, details2: 1, details3: 1, image1: 1, image2: 1, image3: 1, price: 1, service_provider_name: 1, service_provider_image: 1, service_provider_email: 1 }
+            }
+            const result = await serviceCollection.findOne(query, options)
+            res.send(result)
+        })
 
-    // booking//
+        // booking//
 
-    app.post('/bookings', async(req, res)=>{
-        const booking = req.body
-        console.log(booking)
-        const result = await bookingCollection.insertOne(booking)
-        res.send(result)
-    })
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        app.get('/bookings', async (req, res) => {
+            console.log(req.query.userEmail)
+            let query = {}
+            if (req.query?.userEmail){
+                query = {userEmail: req.query.userEmail}
+            }
+            const cursor = bookingCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+        
+
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body
+            const result = await bookingCollection.insertOne(booking)
+            res.send(result)
+        })
+       
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Pest Control server is running')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Pest Control server is running on port ${port}`)
 })
